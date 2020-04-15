cbd97f909038074b95339e7c4d567b8baef9cd3f
Joanne1234 <44009842+Joanne1234@users.noreply.github.com>
2020-04-15 20:44:51 +1000

    Update textToSpeech.py

--- openvinoDemo/textToSpeech.py
+++ openvinoDemo/textToSpeech.py
@@ -2,7 +2,7 @@ import subprocess
 #import pyttsx3
 
 # This script creates wav files required for hazard notification
-# Creates 3 folders: objectsWav, rangesWav, warningsWav
+# Creates 3 folders: objectsWav, numbersWav, warningsWav
 
 # requires pyttsx3 pip3 installed
 # speaks here
@@ -33,18 +33,20 @@ def objectWavFiles():
     for object in objects:
         textToWav(object, "objectsWav")
 
-def rangeWavFiles():
-    ranges=["1", "2", "3", "4"]
-    makeFolder("rangesWav")
-    for range in ranges:
-        textToWav(range+" Metres Away", "rangesWav")
+def numberWavFiles():
+    makeFolder("numbersWav")
+    for num in range(0, 11):
+        textToWav(num, "rangesWav")
+    numbers=[20, 30, 40, 50, 60, 70, 80, 90, 100]
+    for num in numbers:
+	textToWav(num, "rangesWav")
         
 def warningWavFiles():
-    warnings=["Incoming", "Warning", "Hazard", "Approaching"]
+    warnings=["Incoming", "Warning", "Hazard", "Approaching", "Metres Away", "And", "Detected", "No"]
     makeFolder("warningWav")
     for warning in warnings:
         textToWav(warning, "warningWav")     
     
 objectWavFiles()
-rangeWavFiles()
+numberWavFiles()
 warningWavFiles()
